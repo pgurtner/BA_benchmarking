4224a3530d7b6293d359f7d2b3348101e90cccae
pgurtner <81441242+pgurtner@users.noreply.github.com>
2025-02-05 11:05:38 +0100

    feat: add bm, meshgen, bmove aliase

--- aliase.sh
+++ aliase.sh
@@ -1,4 +1,7 @@
+alias bm='python3 main.py benchmark'
 alias init='python3 main.py init'
 alias run='python3 main.py run'
 alias plot='python3 main.py plot'
 alias compare='python3 main.py compare'
+alias meshgen='python3 main.py meshgen'
+alias bmove='python3 main.py move'
